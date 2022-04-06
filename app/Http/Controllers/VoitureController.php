8ff4865d306106e329bb72cbb9b6f92912c97c94
ronphobos <sidibearouna71@gmail.com>
2022-04-06 17:56:54 +0000

    first commit

---
+++ app/Http/Controllers/VoitureController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Voiture;
+use Illuminate\Http\Request;
+
+class VoitureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $voiture = Voiture::get();
+        return view('index', compact('voiture'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        $voiture = Voiture::get();
+        return view('create', compact('voiture'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+            $voi = $this->validate($request, [
+                'nom' => '',
+                'model' => '',
+            ]);
+
+            Voiture::create($voi);
+
+            return redirect()->back();
+
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Voiture  $voiture
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Voiture $voiture)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Voiture  $voiture
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Voiture $voiture)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Voiture  $voiture
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Voiture $voiture)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Voiture  $voiture
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Voiture $voiture)
+    {
+        //
+        $voiture->delete();
+        return redirect()->back();
+    }
+}
